e6e45752f527f04521888ef56a9dff8a4ac22e64
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-10-17 13:30:41 +0500

    update task_manager/statuses/views.py

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from task_manager.statuses.models import Status
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.utils.translation import gettext
+from task_manager.statuses.forms import StatusForm
+
 
 # Create your views here.
+@login_required
+def statuses_list(request):
+    statuses = Status.objects.all()
+    return render(request,
+                  'statuses/statuses_list.html',
+                  {'statuses': statuses})
+
+
+@login_required
+def status_create(request):
+    if request.method == 'POST':
+        form = StatusForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, gettext("Status successfully created"))
+            return redirect('statuses_list')
+    else:
+        form = StatusForm()
+    return render(request, 'statuses/status_form.html', {'form': form})
