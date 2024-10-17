1715f1bf87152693ac0e89c3ed8702a1e9dab845
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-10-17 13:28:30 +0500

    add task_manager/statuses/forms.py

---
+++ task_manager/statuses/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.utils.translation import gettext
+from task_manager.statuses.models import Status
+
+
+class StatusForm(forms.ModelForm):
+    class Meta:
+        model = Status
+        fields = [('name')]
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+        labels = {
+            'name': gettext('status name'),
+        }
