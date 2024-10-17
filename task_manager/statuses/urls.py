a570560fd65a3f14e5abe8d4a0994da8d42f8468
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-10-17 16:24:30 +0500

    update task_manager/statuses/urls.py

--- task_manager/statuses/urls.py
+++ task_manager/statuses/urls.py
@@ -4,4 +4,6 @@ from task_manager.statuses import views
 urlpatterns = [
     path('', views.statuses_list, name='statuses_list'),
     path('create/', views.status_create, name='status_create'),
+    path('<int:pk>/update/', views.status_update, name='status_update'),
+    path('<int:pk>/delete/', views.status_delete, name='status_delete'),
 ]
