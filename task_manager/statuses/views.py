5ee70fc9ffde1e83ed0e988d2145de4e4c7d6dc5
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-10-17 16:24:10 +0500

    update task_manager/statuses/views.py

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from task_manager.statuses.models import Status
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -27,3 +27,38 @@ def status_create(request):
     else:
         form = StatusForm()
     return render(request, 'statuses/status_form.html', {'form': form})
+
+
+@login_required
+def status_update(request, pk):
+    status = get_object_or_404(Status, pk=pk)
+    if request.method == 'POST':
+        form = StatusForm(request.POST, instance=status)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request,
+                gettext("Status is successfully updated"))
+            return redirect('statuses_list')
+    else:
+        form = StatusForm(instance=status)
+    return render(request, 'statuses/status_form.html', {'form': form})
+
+
+@login_required
+def status_delete(request, pk):
+    status = get_object_or_404(Status, pk=pk)
+    # if status.task_set.exists():
+    #    messages.error(
+    #        request,
+    #        gettext("Cannot delete status because it is in use"))
+    #    return redirect('statuses_list')
+    if request.method == 'POST':
+        status.delete()
+        messages.success(
+            request,
+            gettext("Status is successfully deleted"))
+        return redirect('statuses_list')
+    return render(request,
+                  'statuses/status_delete.html',
+                  {'status': status})
