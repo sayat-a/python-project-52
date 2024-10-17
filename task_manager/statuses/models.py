5bfdaa657ba082297e367a694d28882b9e61a6f7
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-10-17 13:30:13 +0500

    update task_manager/statuses/models.py

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils.translation import gettext
+
 
 # Create your models here.
+class Status(models.Model):
+    name = models.CharField(max_length=255,
+                            unique=True,
+                            verbose_name=gettext("Name"))
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
