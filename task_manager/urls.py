0c44cf162ddf03e51c582e011d2207154f8af593
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-10-18 14:16:03 +0500

    update task_manager/urls.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('users/', include('task_manager.users.urls')),
     path('statuses/', include('task_manager.statuses.urls')),
     path('tags/', include('task_manager.tags.urls')),
+    path('tasks/', include('task_manager.tasks.urls')),
 ]
