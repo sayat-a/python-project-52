4de32f184c7e40499a11851a2957717f7002ff32
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-10-19 00:44:59 +0500

    update task_manager/settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'task_manager',
     'task_manager.users',
     'task_manager.statuses',
-    'task_manager.tags',
+    'task_manager.labels',
     'task_manager.tasks',
 ]
 
