7871f3f734d656a9bb4343e53b6894cb0100c581
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-10-23 01:53:40 +0500

    add task_manager/tasks/filter.py

---
+++ task_manager/tasks/filter.py
@@ -0,0 +1,44 @@
+import django_filters
+from django import forms
+from django.contrib.auth.models import User
+from django.forms import widgets
+from django.utils.translation import gettext
+from task_manager.tasks.models import Task
+from task_manager.labels.models import Label
+from task_manager.statuses.models import Status
+
+
+class TaskFilter(django_filters.FilterSet):
+    status = django_filters.ModelChoiceFilter(
+        queryset=Status.objects.all(),
+        label=gettext("Status"),
+        widget=widgets.Select(attrs={'class': 'form-select ml-2 mr-3 mb-3'})
+    )
+
+    executor = django_filters.ModelChoiceFilter(
+        queryset=User.objects.all(),
+        label=gettext("Executor"),
+        widget=widgets.Select(attrs={'class': 'form-select ml-2 mr-3 mb-3'})
+    )
+
+    label = django_filters.ModelChoiceFilter(
+        queryset=Label.objects.all(),
+        label=gettext("Label"),
+        widget=widgets.Select(attrs={'class': 'form-select ml-2 mr-3 mb-3'})
+    )
+
+    filter_own_tasks = django_filters.BooleanFilter(
+        label=gettext("Only own tasks"),
+        method='filter_by_author',
+        widget=forms.CheckboxInput(
+            attrs={'class': 'form-check-input mr-3 mb-4'})
+    )
+
+    class Meta:
+        model = Task
+        fields = ['status', 'executor', 'label', 'filter_own_tasks']
+
+    def filter_by_author(self, queryset, name, value):
+        if value:
+            return queryset.filter(creator=self.request.user)
+        return queryset
