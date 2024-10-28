17e399d7c790856ebbec1cda600c875064f39cb3
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-10-28 17:35:17 +0500

    update task_manager/tasks/forms.py

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -1,13 +1,23 @@
 from django import forms
 from task_manager.tasks.models import Task
 from django.utils.translation import gettext
+from django.contrib.auth.models import User
+
+
+class UserModelChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
 
 
 class TaskForm(forms.ModelForm):
+    executor = UserModelChoiceField(
+        queryset=User.objects.all(),
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'executor', 'labels']
-
         widgets = {
             'name': forms.TextInput(
                 attrs={
@@ -16,12 +26,11 @@ class TaskForm(forms.ModelForm):
             'description': forms.Textarea(
                 attrs={
                     'class': 'form-control',
-                    'rows': 10,
-                    'placeholder': gettext('Description')}),
-            'status': forms.Select(attrs={'class': 'form-control'}),
-            'executor': forms.Select(attrs={'class': 'form-control'}),
+                    'rows': 10, 'placeholder': gettext('Description')}),
+            'status': forms.Select(
+                attrs={
+                    'class': 'form-control'}),
             'labels': forms.SelectMultiple(
                 attrs={
-                    'class': 'form-control',
-                    'size': 4}),
+                    'class': 'form-control', 'size': 4}),
         }
