8c2afad553bcbd3aa334febd5217aad15b25961f
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-10-31 11:24:20 +0500

    update task_manager/statuses/views.py

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,64 +1,54 @@
-from django.shortcuts import render, redirect, get_object_or_404
 from task_manager.statuses.models import Status
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from django.utils.translation import gettext
 from task_manager.statuses.forms import StatusForm
+from task_manager.common_views_services import (
+    object_list,
+    object_create,
+    object_update,
+    object_delete
+)
 
 
 # Create your views here.
-@login_required
 def statuses_list(request):
-    statuses = Status.objects.all()
-    return render(request,
-                  'statuses/statuses_list.html',
-                  {'statuses': statuses})
+    return object_list(
+        request,
+        Status,
+        'statuses/statuses_list.html',
+        'statuses'
+    )
 
 
-@login_required
 def status_create(request):
-    if request.method == 'POST':
-        form = StatusForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request,
-                             gettext("Status is successfully created"))
-            return redirect('statuses_list')
-    else:
-        form = StatusForm()
-    return render(request, 'statuses/status_form.html', {'form': form})
+    return object_create(
+        request,
+        StatusForm,
+        'statuses/status_form.html',
+        'statuses_list',
+        gettext("Status is successfully created")
+    )
 
 
-@login_required
 def status_update(request, pk):
-    status = get_object_or_404(Status, pk=pk)
-    if request.method == 'POST':
-        form = StatusForm(request.POST, instance=status)
-        if form.is_valid():
-            form.save()
-            messages.success(
-                request,
-                gettext("Status is successfully updated"))
-            return redirect('statuses_list')
-    else:
-        form = StatusForm(instance=status)
-    return render(request, 'statuses/status_form.html', {'form': form})
+    return object_update(
+        request,
+        pk,
+        Status,
+        StatusForm,
+        'statuses/status_form.html',
+        'statuses_list',
+        gettext("Status is successfully updated")
+    )
 
 
-@login_required
 def status_delete(request, pk):
-    status = get_object_or_404(Status, pk=pk)
-    if status.task_set.exists():
-        messages.error(
-            request,
-            gettext("Cannot delete status because it is in use"))
-        return redirect('statuses_list')
-    if request.method == 'POST':
-        status.delete()
-        messages.success(
-            request,
-            gettext("Status is successfully deleted"))
-        return redirect('statuses_list')
-    return render(request,
-                  'statuses/status_delete.html',
-                  {'status': status})
+    return object_delete(
+        request,
+        pk,
+        Status,
+        'statuses/status_delete.html',
+        'statuses_list',
+        gettext("Status is successfully deleted"),
+        gettext("Cannot delete status because it is in use"),
+        'task_set'
+    )
