8819fed3f5b51cf1be8df73c05dd1acf4fe450dc
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-10-31 15:48:33 +0500

    update task_manager/statuses/views.py

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -5,7 +5,10 @@ from task_manager.common_views_services import (
     object_list,
     object_create,
     object_update,
-    object_delete
+    object_delete,
+    DeleteConfig,
+    UpdateConfig,
+    CreateConfig
 )
 
 
@@ -20,35 +23,33 @@ def statuses_list(request):
 
 
 def status_create(request):
-    return object_create(
-        request,
-        StatusForm,
-        'statuses/status_form.html',
-        'statuses_list',
-        gettext("Status is successfully created")
+    config = CreateConfig(
+        form_class=StatusForm,
+        template_name='statuses/status_form.html',
+        success_url='statuses_list',
+        success_message=gettext("Status is successfully created")
     )
+    return object_create(request, config)
 
 
 def status_update(request, pk):
-    return object_update(
-        request,
-        pk,
-        Status,
-        StatusForm,
-        'statuses/status_form.html',
-        'statuses_list',
-        gettext("Status is successfully updated")
+    config = UpdateConfig(
+        model=Status,
+        form_class=StatusForm,
+        template_name='statuses/status_form.html',
+        success_url='statuses_list',
+        success_message=gettext("Status is successfully updated")
     )
+    return object_update(request, pk, config)
 
 
 def status_delete(request, pk):
-    return object_delete(
-        request,
-        pk,
-        Status,
-        'statuses/status_delete.html',
-        'statuses_list',
-        gettext("Status is successfully deleted"),
-        gettext("Cannot delete status because it is in use"),
-        'task_set'
+    config = DeleteConfig(
+        model=Status,
+        template_name='statuses/status_delete.html',
+        success_url='statuses_list',
+        success_message=gettext("Status is successfully deleted"),
+        in_use_message=gettext("Cannot delete status because it is in use"),
+        related_field='task_set'
     )
+    return object_delete(request, pk, config)
