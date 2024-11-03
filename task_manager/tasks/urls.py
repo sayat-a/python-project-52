c22ff764d191f86ba3b2b6b6001cfc25894e93e6
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-11-04 02:24:44 +0500

    update task_manager/tasks/urls.py

--- task_manager/tasks/urls.py
+++ task_manager/tasks/urls.py
@@ -4,8 +4,16 @@ from task_manager.tasks import views
 
 urlpatterns = [
     path('', views.TaskListView.as_view(), name='tasks_list'),
-    path('create/', views.task_create, name='task_create'),
-    path('<int:pk>/update/', views.task_update, name='task_update'),
-    path('<int:pk>/delete/', views.task_delete, name='task_delete'),
-    path('<int:pk>/', views.task_detail, name='task_detail'),
+    path('create/', views.TaskCreateView.as_view(), name='task_create'),
+    path(
+        '<int:pk>/update/',
+        views.TaskUpdateView.as_view(),
+        name='task_update'
+    ),
+    path(
+        '<int:pk>/delete/',
+        views.TaskDeleteView.as_view(),
+        name='task_delete'
+    ),
+    path('<int:pk>/', views.TaskDetailView.as_view(), name='task_detail'),
 ]
