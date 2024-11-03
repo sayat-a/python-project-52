839108f0c01a23e363240ff2031c278f9eb65257
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-11-04 02:25:02 +0500

    update task_manager/tasks/views.py

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -1,11 +1,19 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.contrib import messages
 from django.utils.translation import gettext
 from django_filters.views import FilterView
 from task_manager.tasks.models import Task
 from task_manager.tasks.forms import TaskForm
 from task_manager.tasks.filter import TaskFilter
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic import (
+    CreateView,
+    UpdateView,
+    DeleteView,
+    DetailView
+    )
 
 
 # Create your views here.
@@ -21,53 +29,49 @@ class TaskListView(FilterView):
         return filterset
 
 
-@login_required
-def task_create(request):
-    if request.method == 'POST':
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            task = form.save(commit=False)
-            task.creator = request.user
-            task.save()
-            form.save_m2m()
-            messages.success(request, gettext("Task is successfully created"))
-            return redirect('tasks_list')
-    else:
-        form = TaskForm()
-    return render(request, 'tasks/task_form.html', {'form': form})
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'tasks/task_form.html'
+    success_url = reverse_lazy('tasks_list')
+
+    def form_valid(self, form):
+        task = form.save(commit=False)
+        task.creator = self.request.user
+        task.save()
+        form.save_m2m()
+        messages.success(self.request, gettext("Task is successfully created"))
+        return super().form_valid(form)
 
 
-@login_required
-def task_update(request, pk):
-    task = get_object_or_404(Task, pk=pk)
-    if request.method == 'POST':
-        form = TaskForm(request.POST, instance=task)
-        if form.is_valid():
-            form.save()
-            messages.success(request, gettext("Task is successfully updated"))
-            return redirect('tasks_list')
-    else:
-        form = TaskForm(instance=task)
-    return render(request, 'tasks/task_form.html', {'form': form})
+class TaskUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'tasks/task_form.html'
+    success_url = reverse_lazy('tasks_list')
+    success_message = gettext("Task is successfully updated")
 
 
-@login_required
-def task_delete(request, pk):
-    task = get_object_or_404(Task, pk=pk)
-    if request.user != task.creator:
-        messages.error(
-            request,
-            gettext("Task can be deleted only by it's creator"))
+class TaskDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Task
+    template_name = 'tasks/task_delete.html'
+    success_url = reverse_lazy('tasks_list')
+    success_message = gettext("Task is successfully deleted")
+
+    def test_func(self):
+        return self.request.user == self.get_object().creator
+
+    def handle_no_permission(self):
+        messages.error(self.request, gettext(
+            "Task can be deleted only by it's creator"))
         return redirect('tasks_list')
 
-    if request.method == 'POST':
-        task.delete()
-        messages.success(request, gettext("Task is successfully deleted"))
-        return redirect('tasks_list')
-    return render(request, 'tasks/task_delete.html', {'task': task})
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, self.success_message)
+        return super().delete(request, *args, **kwargs)
 
 
-@login_required
-def task_detail(request, pk):
-    task = get_object_or_404(Task, pk=pk)
-    return render(request, 'tasks/task_detail.html', {'task': task})
+class TaskDetailView(LoginRequiredMixin, DetailView):
+    model = Task
+    template_name = 'tasks/task_detail.html'
+    context_object_name = 'task'
