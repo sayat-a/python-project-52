60fb9be161f79e711762b3249a641209e6a3b459
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-11-04 11:20:51 +0500

    update task_manager/tasks/filter.py

--- task_manager/tasks/filter.py
+++ task_manager/tasks/filter.py
@@ -1,6 +1,8 @@
 import django_filters
 from django import forms
 from django.contrib.auth.models import User
+from django.db.models import Value
+from django.db.models.functions import Concat
 from django.forms import widgets
 from django.utils.translation import gettext
 from task_manager.tasks.models import Task
@@ -43,3 +45,9 @@ class TaskFilter(django_filters.FilterSet):
         if value:
             return queryset.filter(creator=self.request.user)
         return queryset
+
+    @property
+    def executor_queryset(self):
+        return User.objects.annotate(
+            full_name=Concat('first_name', Value(' '), 'last_name')
+        )
