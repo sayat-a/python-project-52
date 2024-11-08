0ec5f9596868d097b28578096c5f7c93ff34fb8c
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-11-08 22:16:59 +0500

    update task_manager/views.py

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render
-from django.utils.translation import gettext
+from django.utils.translation import gettext as _
 from django.urls import reverse_lazy
 
 
@@ -9,17 +10,17 @@ def index(request):
     return render(request, 'index.html')
 
 
-class CustomLoginView(LoginView):
+class CustomLoginView(SuccessMessageMixin, LoginView):
     template_name = 'login.html'
     next_page = 'index'
     redirect_authenticated_user = True
+    success_message = _('You have logged in.')
 
     def form_valid(self, form):
-        messages.success(self.request, gettext('You have logged in.'))
         return super().form_valid(form)
 
     def form_invalid(self, form):
-        messages.error(self.request, gettext(
+        messages.error(self.request, _(
             'Please insert the correct username and password. '
             'Both fields may be case sensitive.'))
         return super().form_invalid(form)
@@ -29,5 +30,5 @@ class CustomLogoutView(LogoutView):
     next_page = reverse_lazy('index')
 
     def dispatch(self, request, *args, **kwargs):
-        messages.info(request, gettext('You have logged out.'))
+        messages.info(request, _('You have logged out.'))
         return super().dispatch(request, *args, **kwargs)
