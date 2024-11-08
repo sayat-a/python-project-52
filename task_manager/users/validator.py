d6d3ff1dc726ab03f6b32743ebf4802a503949cb
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-11-08 22:23:07 +0500

    update task_manager/users/validator.py

--- task_manager/users/validator.py
+++ task_manager/users/validator.py
@@ -1,18 +1,18 @@
 from django.core.exceptions import ValidationError
-from django.utils.translation import gettext
+from django.utils.translation import gettext as _
 
 
 class SimplePasswordValidator:
     def validate(self, password, user=None):
         if len(password) < 3:
             raise ValidationError(
-                gettext(
+                _(
                     "The password you entered is too short. "
                     "It must contain at least 3 characters."),
                 code='password_too_short',
             )
 
     def get_help_text(self):
-        return gettext(
+        return _(
             "The password you entered is too short. "
             "It must contain at least 3 characters.")
