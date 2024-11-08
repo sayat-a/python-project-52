f2ab2f4539c33623aa04d5e07d40aad6becc1dd2
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-11-08 22:24:43 +0500

    update task_manager/labels/views.py

--- task_manager/labels/views.py
+++ task_manager/labels/views.py
@@ -2,7 +2,7 @@ from task_manager.labels.models import Label
 from django.urls import reverse_lazy
 from django.shortcuts import redirect
 from django.contrib import messages
-from django.utils.translation import gettext
+from django.utils.translation import gettext as _
 from django.contrib.messages.views import SuccessMessageMixin
 from task_manager.labels.forms import LabelForm
 from django.views.generic import (
@@ -25,7 +25,7 @@ class LabelCreateView(SuccessMessageMixin, CreateView):
     template_name = 'labels/label_form.html'
     form_class = LabelForm
     success_url = reverse_lazy('labels_list')
-    success_message = gettext("Label is successfully created")
+    success_message = _("Label is successfully created")
 
 
 class LabelUpdateView(SuccessMessageMixin, UpdateView):
@@ -33,20 +33,20 @@ class LabelUpdateView(SuccessMessageMixin, UpdateView):
     template_name = 'labels/label_form.html'
     form_class = LabelForm
     success_url = reverse_lazy('labels_list')
-    success_message = gettext("Label is successfully updated")
+    success_message = _("Label is successfully updated")
 
 
 class LabelDeleteView(SuccessMessageMixin, DeleteView):
     model = Label
     template_name = 'labels/label_delete.html'
     success_url = reverse_lazy('labels_list')
-    success_message = gettext("Label is successfully deleted")
+    success_message = _("Label is successfully deleted")
 
     def form_valid(self, form):
         if self.get_object().task_set.exists():
             messages.error(
                 self.request,
-                gettext("Cannot delete label because it is in use")
+                _("Cannot delete label because it is in use")
             )
             return redirect(self.success_url)
         response = super().form_valid(form)
