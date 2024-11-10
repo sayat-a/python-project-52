6fca3d4eb5cba8f38dc57ae0aebd120a24e6676f
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-11-10 19:21:00 +0500

    update task_manager/settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -93,6 +93,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'task_manager.wsgi.application'
 
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
