6ba88799040552b325d5d483cc9de9b4963ef7a6
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-11-10 20:36:36 +0500

    update task_manager/users/models.py

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -3,4 +3,5 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    pass
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
