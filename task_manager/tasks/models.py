647943048af281716f0cd5e36ac599debaaa55c0
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-11-10 19:21:48 +0500

    update task_manager/tasks/models.py

--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from task_manager.users.models import CustomUser
 from task_manager.statuses.models import Status
 from task_manager.labels.models import Label
 
@@ -9,10 +9,10 @@ class Task(models.Model):
     name = models.CharField(max_length=255, unique=True, blank=False)
     description = models.TextField(blank=False)
     status = models.ForeignKey(Status, blank=False, on_delete=models.PROTECT)
-    executor = models.ForeignKey(User, blank=False, on_delete=models.PROTECT)
+    executor = models.ForeignKey(CustomUser, blank=False, on_delete=models.PROTECT)
     labels = models.ManyToManyField(Label, blank=True)
     creator = models.ForeignKey(
-        User, on_delete=models.PROTECT, related_name='created_tasks')
+        CustomUser, on_delete=models.PROTECT, related_name='created_tasks')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
