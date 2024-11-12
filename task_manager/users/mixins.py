c5659da933293ad176470a031d5cac2ffdc18274
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-11-12 11:25:22 +0500

    update task_manager/users/mixins.py

--- task_manager/users/mixins.py
+++ task_manager/users/mixins.py
@@ -6,14 +6,18 @@ from django.utils.translation import gettext as _
 
 class CustomLoginRequiredMixin(LoginRequiredMixin):
     permission_denied_message = _("You're not authenticated! Please, log in.")
+
+    def handle_no_permission(self):
+        messages.error(self.request, self.permission_denied_message)
+        return redirect('login')
+
+
+class SelfCheckMixin:
     permission_check_message = _(
         "You do not have permission to modify another user."
     )
 
     def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_authenticated:
-            messages.error(request, self.permission_denied_message)
-            return redirect('login')
         if hasattr(self, 'get_object') and self.get_object() != request.user:
             messages.error(request, self.permission_check_message)
             return redirect('users')
