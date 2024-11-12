083154d6b8dc205127cf539f226cb26c16befc5f
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2024-11-12 10:07:32 +0500

    update task_manager/labels/models.py - remove comments

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 
-# Create your models here.
 class Label(models.Model):
     name = models.CharField(max_length=255, unique=True,)
     created_at = models.DateTimeField(auto_now_add=True)
